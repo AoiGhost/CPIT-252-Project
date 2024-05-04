6f8e9d4cc8aa9ef22f580ff565394ef7940ccfe9
AoiGhost <aoighostchan@gmail.com>
2024-05-04 11:16:16 +0300

    Initial commit

---
+++ src/cpit/pkg252/project/CPIT252Project.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cpit.pkg252.project;
+
+/**
+ *
+ * @author 96657
+ */
+public class CPIT252Project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //this class will be renamed to fit the registration system project name.
+    }
+    
+}
